f90c76398f8b4d5c287ed2f711b07cedd6988e49
lchaoqun <lchaoqun@sina.cn>
2017-11-24 17:27:32 +0800

    end

--- config/db.js
+++ config/db.js
@@ -3,8 +3,8 @@
  */
 let mysql = require("mysql");
 module.exports =  mysql.createPool({
-    host:     "120.78.152.50",
-    user:     "lin",
-    password: "g7845120",
+    host:     "localhost",
+    user:     "xxx",
+    password: "xxx",
     database: "blog"
 });
