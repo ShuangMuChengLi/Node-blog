d0963e5ac4e265b9f016430bce246b242e2616e0
lchaoqun <lchaoqun@sina.cn>
2017-12-21 18:45:18 +0800

    limit comment times of same ip in a short time

--- config/conf.js
+++ config/conf.js
@@ -1,7 +1,7 @@
 const fileServerPort = "6003";// 文件服务器
 const fileServerIp = "localhost"; // 文件服务器IP
-const fileServerHostname = "http://localhost"; // 文件服务器域名
-// const fileServerHostname = "http://www.linchaoqun.com"; // 文件服务器域名
+// const fileServerHostname = "http://localhost"; // 文件服务器域名
+const fileServerHostname = "http://www.linchaoqun.com"; // 文件服务器域名
 const fileServiceOrigin = fileServerHostname + ":" + fileServerPort;
 const fileServerPathname =  "/upload";
 module.exports={
